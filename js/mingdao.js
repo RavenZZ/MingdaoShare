f83eb4b922ea5e14d24735cf879f5014cbddb636
Jerry <jjkkwwww@163.com>
2014-09-25 09:20:59 +0800

    加入360极速浏览器的支持

--- js/mingdao.js
+++ js/mingdao.js
@@ -1,9 +1,15 @@
 ﻿"use strict"; (function () {
     var domain = "mingdao.com";
-	window.app_key = "FC07433C4B74";
-	window.app_secret = "501C8FA4F7E51BFCAE7EED783653150";
-//	window.app_key = "820FB90D42C2";
-//	window.app_secret = "5F51FFBCC9D52E99E357592374AF8A";
+    //for 测试
+	// window.app_key = "FC07433C4B74";
+	// window.app_secret = "501C8FA4F7E51BFCAE7EED783653150";
+    //for chrome 
+	window.app_key = "820FB90D42C2";
+	window.app_secret = "5F51FFBCC9D52E99E357592374AF8A";
+    //for 360极速 
+    window.app_key = "54F359856C06";
+    window.app_secret = "33FA2BB6A91DC874057AC966E761CBB";
+
     window.response_type = "authorization_code";
     window.callbackUrl = chrome.extension.getURL('index.html');
     window.authorizeUrl = "https://api." + domain + "/oauth2/authorize";
